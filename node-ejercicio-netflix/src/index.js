1af73c1c184418d396d3e4f45c385161d533e12d
sandramarincodes <smarinfernan@gmail.com>
2025-07-22 20:08:30 +0200

    Update index js

--- node-ejercicio-netflix/src/index.js
+++ node-ejercicio-netflix/src/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // create and config server
 const server = express();
@@ -10,4 +10,34 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+const fakeMovies = [
+  {
+    id: 1,
+    title: "Wonder Woman",
+    genre: "Action",
+    image:
+      "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
+    category: "Superhero",
+    year: 2017,
+    director: "Patty Jenkins",
+  },
+  {
+    id: 2,
+    title: "Inception",
+    genre: "Science Fiction",
+    image:
+      "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
+    category: "Thriller",
+    year: 2010,
+    director: "Christopher Nolan",
+  },
+];
+
+server.get("/movies", (req, res) => {
+  res.json({
+    success: true,
+    movies: fakeMovies,
+  });
 });
