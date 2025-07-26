7f84e4453aecf1ce15158ab50febe0479dcdec69
LuciaCarballo <lulucarcas98@gmail.com>
2025-07-26 13:52:51 +0200

    mysql

--- node-ejercicio-netflix/src/index.js
+++ node-ejercicio-netflix/src/index.js
@@ -10,7 +10,7 @@ server.use(cors());
 server.use(express.json());
 
 // init express aplication
-const serverPort = 4001;
+const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
